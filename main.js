7ad13daf4bd83773bf4fab933d02041be4056b8d
labstraction <labstraction@protonmail.com>
2023-02-24 19:15:53 +0100

    new data preparation

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 
 
-const world = await osmService().getOsmData(44.40491870319353, 8.931386063232106, 0.02);
-console.log(world);
+const world = await osmService().getOsmData(44.40491870319353, 8.931386063232106, 0.003);
+drawWorldinCanvas(world, 'osm-canvas');
+
+function drawWorldinCanvas(world, canvas) {
+    console.log('eccp',world);
+    const _canvas = typeof canvas === 'string' ? document.getElementById(canvas) : canvas;
+    const ctx = _canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+
+    
+
+    world.forEach(w => {
+        if (w.type === 'way') {
+            console.log(
+              w.color
+            )
+            ctx.beginPath();
+            
+            w.path.forEach((p, i) => {
+                if (i === 0) {
+                    ctx.moveTo(p.x, p.y);
+                } else {
+                    ctx.lineTo(p.x, p.y);
+                }
+            });
+            ctx.strokeStyle = w.color || 'black';
+            ctx.fillStyle = w.color || 'black';
+            if (w.color === 'red') {
+              ctx.fill();
+            }
+            ctx.lineWidth = 2;
+            ctx.stroke();
+        }
+    });
+
+}
--- osm-service.js
+++ osm-service.js
@@ -1,16 +1,27 @@
 function osmService() {
 
-    function prepareOsmDataForCanvas(osmData, canvas) {
-      if(typeof canvas === 'string'){
-        canvas = document.getElementById(canvas)
-      }
-      const {width, height} = canvas;
+    function prepareOsmData(osmData, lat, lng, renderWidth, renderHeight) {
+        console.log('eccp',osmData);
+        const centerW = renderWidth / 2;
+        const centerH = renderHeight / 2;
+        const scale = 200000;
+        const world = osmData.elements.map(w => {
+            const worldObj = {...w.tags, type: w.type, color: w.tags?.building ? 'red' : 'black'};
+            worldObj.path = w.geometry.map(n => {
+                const x = (n.lon - lng) * scale + centerW;
+                const y = (n.lat - lat) * scale + centerH;
+                return {x, y};
+            });
+            return worldObj;
+        })
+        return world;
     }
 
-    function getOsmData(lat, lon, radius) {
-        const bbox = [lat - radius, lon - radius, lat + radius, lon + radius].join(',');
-        var url = 'http://overpass-api.de/api/interpreter?data=[out:json];way('+ bbox +');out geom;';
+    function getOsmData(lat, lng, radius) {
+        const bbox = [lat - radius, lng - radius, lat + radius, lng + radius].join(',');
+        var url = 'http://overpass-api.de/api/interpreter?data=[out:json];(way["building"]('+ bbox +');way["highway"]('+ bbox +'););out geom;';
         return fetch(url).then((r) => r.json())
+                         .then(d => prepareOsmData(d, lat, lng, 500, 500));
     }
 
     return {
