9355b0d8360118ec5279126471195c4d1ac652cf
labstraction <labstraction@protonmail.com>
2023-02-24 09:34:31 +0100

    query change

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 
 
-const world = await osmService().getOsmData(44.40491870319353, 8.931386063232106, 0.001);
+const world = await osmService().getOsmData(44.40491870319353, 8.931386063232106, 0.02);
+console.log(world);
--- osm-service.js
+++ osm-service.js
@@ -9,7 +9,7 @@ function osmService() {
 
     function getOsmData(lat, lon, radius) {
         const bbox = [lat - radius, lon - radius, lat + radius, lon + radius].join(',');
-        var url = 'http://overpass-api.de/api/interpreter?data=[out:json];node('+ bbox +');way('+ bbox +');out body;>;out skel qt;';
+        var url = 'http://overpass-api.de/api/interpreter?data=[out:json];way('+ bbox +');out geom;';
         return fetch(url).then((r) => r.json())
     }
 
